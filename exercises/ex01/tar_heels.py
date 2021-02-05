598171534751e54847871d805ecd47b43caffda4
Aisha Thompson <a1sha@live.unc.edu>
2021-02-04 22:49:21 -0500

    i think i did it

--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -5,3 +5,15 @@ __author__ = "730318766"
 
 # Begin your solution here...
 arithmetic: int = int(input("Enter an interger: "))
+
+if arithmetic % 2 == 0 and arithmetic % 7 == 0:
+    print("TAR HEELS")
+else:
+    if arithmetic % 2 == 0:
+        print("TAR")
+    else:
+        if arithmetic % 7 == 0:
+            print("HEELS")
+        else:
+            if arithmetic % 2 != 0 and arithmetic % 7 != 0:
+                print("CAROLINA")
