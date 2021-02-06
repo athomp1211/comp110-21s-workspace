f8c11f13dab6ca6590aa2ef68430e933aa7b9d96
Aisha Thompson <a1sha@live.unc.edu>
2021-02-06 12:48:55 -0500

    a

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -1,6 +1,6 @@
 """A vaccination calculator."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730318766"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
@@ -18,3 +18,7 @@ from datetime import timedelta
 
 
 # Begin your solution here...
+int(input("Population: "))
+int(input("Doses administered: "))
+int(input("Doses per day: "))
+int(input("Target percent vaccinated: "))
