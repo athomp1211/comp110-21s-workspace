828f5779fd890a3114bb6b22683989c67dd995d6
Aisha Thompson <a1sha@live.unc.edu>
2021-02-10 18:17:11 -0500

    First Try

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -1,18 +1,32 @@
 """Fortune cookie exercise redux as a structured program."""
 
 from random import randint
+from typing import NoReturn
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730318766"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
 
-# TODO 1: Define your fortune_cookie function here.
+def fortune_cookie() -> str:
+    """Returns random fortune."""
+    rand_fortune: int = int(randint(1, 4))
+    if rand_fortune == 1:
+        return ("A truly rich life contains love and art in abundance.")
+    else:
+        if rand_fortune == 2:
+            return ("One can never fill another’s shoes, rather he must outgrow the old shoes.")
+        else:
+            if rand_fortune == 3:
+                return ("Soon life will become more interesting.")
+            else:
+                if rand_fortune == 4:
+                    return ("Your love life will be happy and harmonious.")
 
 
 # Python Idiom for "starting" the program when run as a module.
