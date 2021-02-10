679f55503b01fe4bdf6798de4527dd9559283728
Aisha Thompson <a1sha@live.unc.edu>
2021-02-10 18:45:29 -0500

    Not Done

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,15 +1,28 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730318766"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heels(arithmetic: int))
 
 
-# TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(arithmetic: int) -> str:
+    """Converts booleans into corresponding TarHeel statement."""
+    arithmetic: int = int(input("Enter an interger: "))
+    if arithmetic % 2 == 0 and arithmetic % 7 == 0:
+        return ("TAR HEELS")
+    else:
+        if arithmetic % 2 == 0:
+            return ("TAR")
+        else:
+            if arithmetic % 7 == 0:
+                return ("HEELS")
+            else:
+                if arithmetic % 2 != 0 and arithmetic % 7 != 0:
+                    return ("CAROLINA")
 
 
 if __name__ == "__main__":
