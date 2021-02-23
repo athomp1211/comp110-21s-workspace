f5837c91d507b288ba4ad0eae58d1cb4a6372550
Aisha Thompson <a1sha@live.unc.edu>
2021-02-23 16:18:22 -0500

    1

--- lessons/list_examples.py
+++ lessons/list_examples.py
@@ -7,5 +7,13 @@ rolls = [2, 3, 2, 6] # Initialized w/ list literal syntax
 print(f"Length of rolls is {len(rolls)}")
 
 from random import randint
-rolls.append(randint(1, 6))
+rolls.append(randint(1, 6)) #Lists's append method adds an item to the end of a list
 print(rolls)
+
+rolls.pop() # With no argument, removes the last item of the list
+rolls.pop(0) # With one argument, removes specific index
+print(rolls)
+
+words: list[str] = list() # Construct an empty list using the list constructor
+words.append("Hello")
+print(words)
