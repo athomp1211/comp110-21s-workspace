84d7664422e7dc39c7e53b28a20a33f95f0cde22
Aisha Thompson <a1sha@live.unc.edu>
2021-03-22 23:40:55 -0400

    progress

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -23,6 +23,27 @@ def column_values(table: list[dict[str, str]], column_name: str) -> list[str]:
     """Produces a list[str] of all values in a single column."""
     new_table: list[str] = []
     for row in table:
-        for key in row[column_name]:
-            new_table.append(row[column_name])
-    return new_table 
+        new_table.append(row[column_name])
+    return new_table
+
+
+def columnar(origin_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transforms a table represented as a list of rows into one represented as a dictionary of columns."""
+    column_dict: dict[str, list[str]] = {}
+    for row in origin_table:
+        for key in row:
+            column_dict[key] = column_values(origin_table, key)
+    return column_dict
+
+
+def head(non_mutated: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """Produce a new column-based table with only the first N rows of data for each column."""
+    top_rows: dict[str, list[str]] = {}
+    for column in non_mutated:
+        first_N: list[str] = []
+        i: int = 0
+        while i <= N:
+            first_N.append(column)
+            i += 1
+        top_rows[] = first_N
+    return top_rows
