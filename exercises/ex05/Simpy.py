11fb8109aa70f4b8b451eff95ffc11aa0027f0a6
Aisha Thompson <a1sha@live.unc.edu>
2021-04-15 00:55:21 -0400

    started

--- exercises/ex05/Simpy.py
+++ exercises/ex05/Simpy.py
@@ -4,10 +4,18 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730318766"
 
 
 class Simpy:
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, values: list[float]):
+        """Initialize the values attribute of a newly constructed Simpy object."""
+        self.values = values
+
+    def __repr__(self) -> str:
+        """Magic method to represent object as string."""
+        return f"Simpy({self.values})" 
+
+    def fill()
